7178dcf1bb9d67fec0e7f19fde2d30c05c14cce8
suhalin61 <147693645+suhalin61@users.noreply.github.com>
2023-10-29 17:39:07 +0530

    Add files via upload

---
+++ find +and-num.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the value");
+scanf("%d",&a);
+if (a>0)
+{
+    printf("number is positive");
+}
+else if (a<0)
+{
+    printf("number is negetive");
+}
+else{
+    printf("numer is equal");
+}
+
+
+}
+
+
+
--- pattern.c
+++ pattern.c
@@ -3,17 +3,25 @@ int main()
 {
   int n,i,j;
   scanf("%d",&n);
-  for(i=0;i<n;i++ )
+  for(i=0;i<n;i++)
   {
-  for(j=0;j<=i;j++)
 
+  for(j=0;j<n;j++)
+  {
+  if(j>=0&& j<=i)
   {
     printf("*");
 
   }
-  printf("\n");
-  }
+else
+     
+  {
+    printf(" ");
 
+  }
+  }
+  printf("\n");
+}
 }
 
 
